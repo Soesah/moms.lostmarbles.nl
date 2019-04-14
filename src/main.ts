56c15e32925fce080c4dde7f0583cda6a25588f1
Carl Giesberts <carl@front-crafter.nl>
2019-04-14 12:49:32 +0200

    Intercapt 401 responses and redirect to home

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import axios from 'axios';
 import {
   longDateFilter,
   dateFilter,
 } from '@/components/common/filters/longDate.filter';
+
+axios.interceptors.response.use(undefined, (err) => {
+  if (err.response.status === 401) {
+    document.location.href = '/';
+  }
+});
 
 Vue.config.productionTip = false;
 

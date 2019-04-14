1c3bfb8ac86d2ecc23d8b115f584544daaa48f55
Carl Giesberts <carl@front-crafter.nl>
2019-04-14 12:05:15 +0200

    Show a text while loading recipes

--- src/components/list/RecipeList.vue
+++ src/components/list/RecipeList.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="box">
     <h2>{{ categoryName() || 'Recepten'}}</h2>
+    <p v-if="loading">De recepten worden geladen...</p>
     <ol>
       <li v-for="recipe in filteredRecipes" :key="recipe.id">
         <router-link :to="`/recipe/${recipe.slug}`" v-text="recipe.name"></router-link>
@@ -13,12 +14,18 @@ import { mapGetters } from 'vuex';
 
 export default {
   name: 'RecipeList',
+  data() {
+    return {
+      loading: true,
+    };
+  },
   computed: {
     ...mapGetters(['categoryName']),
     ...mapGetters(['filteredRecipes']),
   },
-  created() {
-    this.$store.dispatch('getRecipes');
+  async created() {
+    await this.$store.dispatch('getRecipes');
+    this.loading = false;
   },
 };
 </script>

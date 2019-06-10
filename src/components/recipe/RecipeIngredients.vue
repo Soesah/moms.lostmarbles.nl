a5e4161b0cb6e242cf724646fbf44901e65d4d7e
Carl Giesberts <carl@front-crafter.nl>
2019-06-10 21:18:16 +0200

    Fixed display of ingredients

--- src/components/recipe/RecipeIngredients.vue
+++ src/components/recipe/RecipeIngredients.vue
@@ -3,9 +3,9 @@
     <h3>Ingrediënten</h3>
     <ul>
       <li class="ingredient" v-for="(ingredient, index) in recipe.ingredients" :key="index">
-        <span class="amount" v-if="ingredient.amount" v-text="`${ingredient.amount} `"></span>
+        <span class="amount" v-if="ingredient.amount" v-text="ingredient.amount"></span>
         <span class="name" v-text="ingredient.name"></span>
-        <span class="remark" v-if="ingredient.remark" v-text="`, ${ingredient.remark}`"></span>
+        <span class="remark" v-if="ingredient.remark" v-text="ingredient.remark"></span>
       </li>
     </ul>
   </section>

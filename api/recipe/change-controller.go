1ebf1f2b9e59f39f9d35ecef57abf960385fbf34
Carl Giesberts <carl@front-crafter.nl>
2019-10-24 16:53:32 +0200

    Moved changelog to storage api

---
+++ api/recipe/change-controller.go
@@ -0,0 +1,89 @@
+package recipe
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/Soesah/moms.lostmarbles.nl/api/models"
+	"github.com/Soesah/moms.lostmarbles.nl/api/storage"
+)
+
+var (
+	errNoChangesToSave = errors.New("No changes to be saved")
+)
+
+// ChangeController is used to save and load changelog
+type ChangeController struct {
+	NewID   int64
+	Changes []models.ChangeLog
+}
+
+// Load is used to load a recipe list
+func (c *ChangeController) Load(r *http.Request) error {
+	var changelog []models.ChangeLog
+
+	data, err := storage.GetFile("changelog", r)
+
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, &changelog)
+
+	if err != nil {
+		return err
+	}
+
+	c.setList(changelog)
+
+	return nil
+}
+
+// setList is used to start the controller
+func (c *ChangeController) setList(list []models.ChangeLog) {
+	c.Changes = list
+	c.parseNewID()
+}
+
+// getNewID raises the internal ID and returns a new one
+func (c *ChangeController) getNewID() int64 {
+
+	c.NewID = c.NewID + 1
+	return c.NewID
+}
+
+// parseNewID reads the hours and sets newID
+func (c *ChangeController) parseNewID() {
+	var newID int64 = 0
+
+	for _, r := range c.Changes {
+		if r.ID > newID {
+			newID = r.ID
+		}
+	}
+
+	c.NewID = newID
+}
+
+// Store is used to store a changelog
+func (c *ChangeController) Store(r *http.Request) error {
+
+	if len(c.Changes) == 0 {
+		return errNoChangesToSave
+	}
+
+	data, err := json.MarshalIndent(c.Changes, "", "  ")
+
+	if err != nil {
+		return err
+	}
+
+	err = storage.PutFile("changelog", data, r)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- api/recipe/changeapi.go
+++ api/recipe/changeapi.go
@@ -3,43 +3,40 @@ package recipe
 import (
 	"net/http"
 
-	"github.com/Soesah/moms.lostmarbles.nl/api"
 	"github.com/Soesah/moms.lostmarbles.nl/api/models"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/datastore"
 )
 
 // GetLatestChange returns the latest change
 func GetLatestChange(r *http.Request) (models.ChangeLog, error) {
-	ctx := appengine.NewContext(r)
-	var changes []models.ChangeLog
 	var change models.ChangeLog
 
-	q := datastore.NewQuery(api.ChangeLogKind).Order("-Date")
-
-	_, err := q.GetAll(ctx, &changes)
+	c := ChangeController{}
+	err := c.Load(r)
 
 	if err != nil {
 		return change, err
 	}
 
-	change = changes[0]
+	change = c.Changes[0]
 
 	return change, nil
 }
 
 // GetRecipeChanges returns the latest changes for a recipe
-func GetRecipeChanges(id int64, r *http.Request) ([]models.ChangeLog, error) {
-	ctx := appengine.NewContext(r)
-	key := api.RecipeChangeKey(ctx, id)
+func GetRecipeChanges(ID int64, r *http.Request) ([]models.ChangeLog, error) {
 	var changes []models.ChangeLog
 
-	q := datastore.NewQuery(api.ChangeLogKind).Ancestor(key).Order("Date")
-
-	_, err := q.GetAll(ctx, &changes)
+	c := ChangeController{}
+	err := c.Load(r)
 
 	if err != nil {
 		return changes, err
+	}
+
+	for _, change := range c.Changes {
+		if change.RecipeID == ID {
+			changes = append(changes, change)
+		}
 	}
 
 	return changes, nil

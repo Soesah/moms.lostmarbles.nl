01e5576ce29c508bcc9cb814934e3c537f8c5b10
Carl Giesberts <carl@front-crafter.nl>
2019-10-25 10:15:58 +0200

    Show version on page

--- src/components/common/PageTitle.vue
+++ src/components/common/PageTitle.vue
@@ -1,13 +1,21 @@
 <template>
   <header>
     <section class="title">
-      <h1>Moms Lost Marbles</h1>
+      <h1>
+        Moms Lost Marbles
+        <version></version>
+      </h1>
       <p class="tagline">Recepten van de familie Giesberts, Drost, Weber en anderen</p>
     </section>
   </header>
 </template>
 <script>
+import Version from '@/components/common/Version.vue';
+
 export default {
   name: 'PageTitle',
+  components: {
+    Version,
+  },
 };
 </script>
---
+++ src/components/common/Version.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="version" v-text="version"></span>
+</template>
+<script>
+import versionInfo from '@/version.json';
+import { Version } from '@/models/version.model';
+
+export default {
+  name: 'Version',
+  data() {
+    return {
+      v: new Version(versionInfo),
+    };
+  },
+  computed: {
+    version() {
+      return `Version ${this.v.version}`;
+    },
+  },
+};
+</script>

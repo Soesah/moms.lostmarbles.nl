b2aa4bd206a31dcdecb9ef40984ef4a7c82abe34
Carl Giesberts <carl@front-crafter.nl>
2019-10-25 10:30:01 +0200

    Fixed sorting of changes for latest change

--- api/recipe/changeapi.go
+++ api/recipe/changeapi.go
@@ -2,6 +2,7 @@ package recipe
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/Soesah/moms.lostmarbles.nl/api/models"
 )
@@ -16,6 +17,10 @@ func GetLatestChange(r *http.Request) (models.ChangeLog, error) {
 	if err != nil {
 		return change, err
 	}
+
+	sort.SliceStable(c.Changes, func(i, j int) bool {
+		return c.Changes[i].Date.After(c.Changes[j].Date)
+	})
 
 	change = c.Changes[0]
 

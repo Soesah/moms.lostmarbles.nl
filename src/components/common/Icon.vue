a1d233f4ba575b514552bcb1c7b76a625a5cb9d7
Carl Giesberts <carl@front-crafter.nl>
2022-03-10 22:41:27 +0100

    Refactored Icon to Vue3 composition api

--- src/components/common/Icon.vue
+++ src/components/common/Icon.vue
@@ -1,19 +1,13 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const { name } = defineProps({
+  name: {
+    type: String,
+  },
+});
+const img = computed(() => `/img/icons/${name}.png`);
+</script>
 <template>
   <img class="box-icon" width="70" :src="img" />
 </template>
-
-<script>
-export default {
-  name: 'Icon',
-  props: {
-    name: {
-      type: String,
-    },
-  },
-  computed: {
-    img() {
-      return `/img/icons/${this.name}.png`;
-    },
-  },
-};
-</script>
